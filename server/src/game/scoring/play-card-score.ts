1d0ee24eaa8b931a973119ad5762b1110ac5d57d
Jan Zípek <jan@zipek.cz>
2020-04-24 20:18:45 +0200

    miss

--- server/src/game/scoring/play-card-score.ts
+++ server/src/game/scoring/play-card-score.ts
@@ -1,4 +1,6 @@
 import { ScoringContext } from './types'
 import { Card } from '@shared/cards'
 
-export const playCardScore = (ctx: ScoringContext, card: Card) => {}
+export const playCardScore = (ctx: ScoringContext, card: Card) => {
+	return card.resource
+}
