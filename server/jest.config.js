c3d3aca1fafaeae1a2c14438286b8d34b9c2e739
Jan Zípek <jan@zipek.cz>
2020-05-12 18:18:52 +0200

    Added more tests

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
+	testMatch: ['**/__tests__/**/*.test.ts?(x)'],
 	moduleNameMapper: {
 		'^@shared/(.*)$': '<rootDir>../shared/src/$1',
 		'^@/(.*)$': '<rootDir>/src/$1'
---
+++ server/src/utils/tests.ts
@@ -0,0 +1,24 @@
+import { Game } from '@/game/game'
+import { Player } from '@/game/player'
+import { PlayerBaseAction } from '@/game/player/action'
+import { StartGameAction } from '@/game/player/actions/start-game'
+
+export const prepareGame = (
+	actionBuilder?: (p: Player) => PlayerBaseAction
+) => {
+	const game = new Game()
+	const player = new Player(game)
+	game.add(player, false)
+
+	const action = actionBuilder
+		? actionBuilder(player)
+		: new StartGameAction(player)
+
+	return {
+		game: game.state,
+		player: player.state,
+		gameObj: game,
+		playerObj: player,
+		action
+	}
+}
