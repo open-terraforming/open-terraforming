129b71d56c615ea0acefa5fa3a592d9b70b5e439
Jan Zípek <jan@zipek.cz>
2024-08-31 12:25:42 +0200

    Proper signal handling for main server

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,8 +2,11 @@ import yargs from 'yargs'
 import { globalConfig } from './config'
 import { httpServer } from './server/http/http-server'
 import { ServerOptions } from './server/types'
+import { NodeLogger } from './lib/node-logger'
 
 export async function main() {
+	const logger = new NodeLogger('Main')
+
 	const argv = await yargs
 		.scriptName('card-game-server')
 		.command('', 'Starts the server')
@@ -24,6 +27,15 @@ export async function main() {
 	}
 
 	const serverStart = await httpServer(serverConfig)
+
+	process.on('SIGTERM', () => {
+		logger.info('SIGTERM signal received, shutting down')
+
+		serverStart.server.close(() => {
+			logger.info('Server closed, exiting')
+			process.exit(0)
+		})
+	})
 
 	return { port: serverStart.port }
 }
