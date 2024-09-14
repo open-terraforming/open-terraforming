b730eb18145fbdc44c048cdc2a3f67d2ac645b36
Jan Zípek <jan@zipek.cz>
2024-09-14 22:13:39 +0200

    Fix colony steps being too big

--- shared/src/game/game/colonies-production-game-state.ts
+++ shared/src/game/game/colonies-production-game-state.ts
@@ -1,5 +1,6 @@
 import { GameStateValue } from '@shared/game'
 import { BaseGameState } from './base-game-state'
+import { ColoniesLookupApi } from '@shared/expansions/colonies/ColoniesLookupApi'
 
 export class ColoniesProductionGameState extends BaseGameState {
 	name = GameStateValue.ColoniesProduction
@@ -7,7 +8,11 @@ export class ColoniesProductionGameState extends BaseGameState {
 	onEnter() {
 		for (const colony of this.game.state.colonies) {
 			if (colony.active) {
-				colony.step++
+				colony.step = Math.min(
+					colony.step + 1,
+					ColoniesLookupApi.get(colony.code).tradeIncome.slots.length - 1,
+				)
+
 				colony.currentlyTradingPlayer = undefined
 			}
 		}
