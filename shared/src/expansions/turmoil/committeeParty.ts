88e160d25745ea54bae9da6eb30441a947a527d5
Jan Zípek <jan@zipek.cz>
2024-11-23 21:44:24 +0100

    Remove notes, add notes

--- shared/src/expansions/turmoil/committeeParty.ts
+++ shared/src/expansions/turmoil/committeeParty.ts
@@ -1,16 +1,6 @@
 import { CardSymbol } from '@shared/cards'
 import { GameState, GridCell, PlayerState } from '@shared/index'
 
-/**
- * POLICIES:
- *  - gain 1 steel when placing tile on MARS
- *  - you can pay $10 to increase your heat and energy production by 1, may be used as many times as you want
- *  - pay $10 to draw 3 cards, can only be used once per generation per player
- *  - when you gain TR, you loose $3 - if you don't have $3, your TR won't be raised
- *  - titanium is worth $1 more
- *  - gain $4 every time you place a greenery tile
- */
-
 export interface CommitteeParty {
 	code: string
 	bonus: {
@@ -19,7 +9,8 @@ export interface CommitteeParty {
 		apply: (game: GameState) => void
 	}
 	policy: {
-		active: CommitteePartyActiveAction[]
+		// NOTE: The system currently only supports one activate policy at a time
+		active: [CommitteePartyActiveAction]
 		passive: CommitteePartyPassiveAction[]
 	}
 }
