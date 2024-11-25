646cc90cdcb843b30af8bd8b4772165f6d5f2c09
Jan Zípek <jan@zipek.cz>
2024-11-25 21:15:35 +0100

    Better implementation

--- shared/src/game/player/actions/add-delegate-to-party-action-handler.ts
+++ shared/src/game/player/actions/add-delegate-to-party-action-handler.ts
@@ -15,42 +15,29 @@ export class AddDelegateToPartyActionHandler extends PlayerBaseActionHandler<Arg
 			(delegate) => delegate.id === this.player.id,
 		)
 
-		if (lobbyIndex >= 0) {
-			const collector = this.parent.game.startEventsCollector()
-
-			this.game.committee.lobby.splice(lobbyIndex, 1)
-			addDelegate(this.game, party, { id: this.player.id })
-
-			collector.collectAndPush((changes) => ({
-				type: EventType.PlayerMovedDelegate,
-				playerId: this.player.id,
-				changes,
-				change: 1,
-				partyCode: party,
-			}))
-
-			return
-		}
-
-		const reserveIndex = this.game.committee.reserve.findIndex(
-			(delegate) => delegate?.id === this.player.id,
-		)
-
-		if (reserveIndex < 0) {
-			throw new Error("Player doesn't have any delegates left")
-		}
-
-		if (this.player.money < 5) {
-			throw new Error(
-				'Player does not have enough money (5) to add delegate to party',
-			)
-		}
-
 		const collector = this.parent.game.startEventsCollector()
 
-		this.player.money -= 5
+		if (lobbyIndex >= 0) {
+			this.game.committee.lobby.splice(lobbyIndex, 1)
+		} else {
+			if (this.player.money < 5) {
+				throw new Error(
+					'Player does not have enough money (5) to add delegate to party',
+				)
+			}
 
-		this.game.committee.reserve.splice(reserveIndex, 1)
+			const reserveIndex = this.game.committee.reserve.findIndex(
+				(delegate) => delegate?.id === this.player.id,
+			)
+
+			if (reserveIndex < 0) {
+				throw new Error("Player doesn't have any delegates left")
+			}
+
+			this.player.money -= 5
+			this.game.committee.reserve.splice(reserveIndex, 1)
+		}
+
 		addDelegate(this.game, party, { id: this.player.id })
 
 		collector.collectAndPush((changes) => ({
