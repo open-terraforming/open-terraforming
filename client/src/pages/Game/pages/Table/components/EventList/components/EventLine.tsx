ab43729dea6826f8a2665df7405bbd1631800494
Jan Zípek <jan@zipek.cz>
2024-12-09 17:12:22 +0100

    Display time in events

--- client/src/pages/Game/pages/Table/components/EventList/components/EventLine.tsx
+++ client/src/pages/Game/pages/Table/components/EventList/components/EventLine.tsx
@@ -70,10 +70,11 @@ const ColonySpan = ({ colony }: { colony: ColonyState }) => {
 }
 
 const formatTime = (time: number) => {
-	const minutes = Math.floor(time / 60000)
+	const hours = Math.floor(time / 3600000)
+	const minutes = Math.floor((time % 3600000) / 60000)
 	const seconds = ((time % 60000) / 1000).toFixed(0)
 
-	return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+	return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
 }
 
 export const EventLine = ({ event, animated, onDone, timestamp }: Props) => {
