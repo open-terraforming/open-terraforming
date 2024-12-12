9bc77609e0f9fee1a49a46447cb19bd3405f0c59
Jan Zípek <jan@zipek.cz>
2024-12-12 13:42:58 +0100

    Fix typing, remove unused code

--- shared/src/utils/getProtocolDiff.ts
+++ shared/src/utils/getProtocolDiff.ts
@@ -1,82 +1,96 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-export type ProtocolDiffValue<Type> = { v: Type } | { s: Type }
+export type ProtocolDiffValue<Type> = { v: ProtocolDiff<Type> } | { s: Type }
 
 export type ProtocolDelete = null
 
-export type ProtocolDiff<TSource> = TSource extends object
-	? { [K in keyof TSource]?: ProtocolDiffItem<TSource[K]> }
-	: TSource
-
-export type ProtocolDiffItem<TSource> = TSource extends
+export type ProtocolDiff<TSource> = TSource extends
 	| string
 	| number
 	| boolean
 	| symbol
 	| null
-	? ProtocolDiffValue<TSource> | ProtocolDelete
+	? TSource
 	: TSource extends Array<infer T>
-		? ProtocolDiffValue<{
+		? {
 				[K in number | 'length']?: ProtocolDiffValue<T> | ProtocolDelete
-			}>
+			}
 		: TSource extends object
-			? ProtocolDiffValue<{
-					[K in keyof TSource]?: ProtocolDiffItem<TSource[K]>
-				}>
+			? {
+					[K in keyof TSource]?: ProtocolDiffValue<TSource[K]> | ProtocolDelete
+				}
 			: TSource
 
-const valueDiff = (
-	aValue: any,
-	bValue: any,
-): [boolean, ({ v: any } | { s: any })?] => {
+const valueDiff = <TValue>(
+	aValue: TValue,
+	bValue: TValue,
+): [boolean, ({ v: ProtocolDiff<TValue> } | { s: TValue })?] => {
+	// Type mismatch = always different
 	if (
 		typeof aValue !== typeof bValue ||
 		(Array.isArray(aValue) && !Array.isArray(bValue)) ||
 		(!Array.isArray(aValue) && Array.isArray(bValue))
 	) {
 		return [true, { s: bValue }]
-	} else {
-		if (typeof aValue === 'undefined') {
-			return [false]
+	}
+
+	// Both nil the same way - no difference
+	if (
+		(aValue === undefined && bValue === undefined) ||
+		(aValue === null && bValue === null)
+	) {
+		return [false]
+	}
+
+	// Scalars should be compared directly
+	if (typeof aValue !== 'object' && aValue !== bValue) {
+		return [true, { s: bValue }]
+	}
+
+	// One is null, but the other one isn't
+	if ((aValue === null || bValue === null) && aValue !== bValue) {
+		return [true, { s: bValue }]
+	}
+
+	// Compare two arrays
+	if (Array.isArray(aValue)) {
+		const diff = {} as Record<string | number, unknown>
+
+		if (!Array.isArray(bValue)) {
+			throw new Error('Array mismatch')
 		}
 
-		if (typeof aValue !== 'object' || aValue === null || bValue === null) {
-			if (aValue !== bValue) {
-				return [true, { s: bValue }]
-			}
-		} else {
-			if (!Array.isArray(aValue)) {
-				const changes = getProtocolDiff(aValue, bValue)
+		for (let i = 0; i < Math.max(aValue.length, bValue.length); i++) {
+			const [hasDiff, diffValue] = valueDiff(aValue[i], bValue[i])
 
-				if (Object.keys(changes).length > 0) {
-					return [true, { v: changes }]
-				}
-			} else {
-				const diff = {} as Record<string, unknown>
-
-				for (let i = 0; i < Math.max(aValue.length, bValue.length); i++) {
-					const [hasDiff, diffValue] = valueDiff(aValue[i], bValue[i])
-
-					if (hasDiff) {
-						if (
-							diffValue === undefined ||
-							('s' in diffValue && diffValue.s === undefined)
-						) {
-							diff[i] = null
-						} else {
-							diff[i] = diffValue
-						}
-					}
-				}
-
-				if (aValue.length !== bValue.length) {
-					diff.length = { v: bValue.length }
-				}
-
-				if (Object.keys(diff).length > 0) {
-					return [true, { v: diff }]
+			if (hasDiff) {
+				if (
+					diffValue === undefined ||
+					('s' in diffValue && diffValue.s === undefined)
+				) {
+					diff[i] = null
+				} else {
+					diff[i] = diffValue
 				}
 			}
 		}
+
+		if (aValue.length !== bValue.length) {
+			diff.length = { v: bValue.length }
+		}
+
+		if (Object.keys(diff).length > 0) {
+			return [true, { v: diff as ProtocolDiff<TValue> }]
+		}
+	}
+
+	// Compare two objects
+	const changes = getProtocolDiff(
+		aValue as Record<string | number | symbol, any>,
+		bValue as Record<string | number | symbol, any>,
+	)
+
+	if (Object.keys(changes).length > 0) {
+		return [true, { v: changes as ProtocolDiff<TValue> }]
 	}
 
 	return [false]
