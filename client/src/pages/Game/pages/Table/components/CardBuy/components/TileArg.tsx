845772e7fd8f57faaeca021ff928fb93749e14ad
Jan Zípek <jan@zipek.cz>
2024-12-13 14:57:24 +0100

    Remember the location

--- client/src/pages/Game/pages/Table/components/CardBuy/components/TileArg.tsx
+++ client/src/pages/Game/pages/Table/components/CardBuy/components/TileArg.tsx
@@ -3,21 +3,22 @@ import { setTableState } from '@/store/modules/table'
 import { useAppDispatch } from '@/utils/hooks'
 import { CardEffectArgument } from '@shared/cards'
 import { GridCellLocation } from '@shared/gameState'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { TileIcon } from '../../TileIcon/TileIcon'
 import { ArgContainer } from './ArgContainer'
 import { Flex } from '@/components/Flex/Flex'
 
+type TileArgValue = [x: number, y: number, location: GridCellLocation | null]
+
 type Props = {
 	arg: CardEffectArgument
-	onChange: (
-		v: [x: number, y: number, location: GridCellLocation | null],
-	) => void
+	onChange: (v: TileArgValue) => void
 }
 
 export const TileArg = ({ arg, onChange }: Props) => {
 	const dispatch = useAppDispatch()
 	const { tilePlacementState } = arg
+	const [location, setLocation] = useState<TileArgValue>()
 
 	if (!tilePlacementState) {
 		throw new Error('TileArg: No tile placement state')
@@ -33,6 +34,7 @@ export const TileArg = ({ arg, onChange }: Props) => {
 					state: tilePlacementState,
 					onPick: (x, y, location) => {
 						onChangeRef.current([x, y, location])
+						setLocation([x, y, location])
 					},
 				},
 			}),
@@ -46,7 +48,16 @@ export const TileArg = ({ arg, onChange }: Props) => {
 
 				<TileIcon content={tilePlacementState.type} />
 
-				<Button onClick={handlePick}>Pick location</Button>
+				{!location && <Button onClick={handlePick}>Pick location</Button>}
+
+				{location && (
+					<>
+						<div>
+							at {location[0]},{location[1]}
+						</div>
+						<Button onClick={handlePick}>Change</Button>
+					</>
+				)}
 
 				<span>{arg.descriptionPostfix}</span>
 			</Flex>
