ec8120fc2792d4858524afb380c4e2d92249a69e
Jan Zípek <jan@zipek.cz>
2024-12-26 11:32:26 +0100

    Use msgpack to make local games smaller

--- client/src/utils/localGamesStore.ts
+++ client/src/utils/localGamesStore.ts
@@ -1,9 +1,13 @@
 import { GameConfig } from '@shared/game/game'
 import { GameState } from '@shared/gameState'
+import { stripUndefined } from '@shared/utils'
+import { decode, encode } from 'msgpack-lite'
 
 const LOCAL_GAMES_PREFIX = 'ot-local-'
 
 export const extractGameIdFromLocal = (gameId: string) => gameId.split('/')[1]
+
+// TODO: Handle local storage file size limits somehow
 
 export const localGamesStore = {
 	getGame(gameId: string) {
@@ -13,11 +17,11 @@ export const localGamesStore = {
 			return null
 		}
 
-		return JSON.parse(data) as { state: GameState; config: GameConfig }
+		return decode(data) as { state: GameState; config: GameConfig }
 	},
 
 	setGame(gameId: string, data: { state: GameState; config: GameConfig }) {
-		localStorage[LOCAL_GAMES_PREFIX + gameId] = JSON.stringify(data)
+		localStorage[LOCAL_GAMES_PREFIX + gameId] = encode(stripUndefined(data))
 	},
 
 	removeGame(gameId: string) {
