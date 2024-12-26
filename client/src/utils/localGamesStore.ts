5f9465861c08c64397da424913bc20a2f9dceac9
Jan Zípek <jan@zipek.cz>
2024-12-26 11:40:22 +0100

    Use msgpack to encode and decode saved local games

--- client/src/utils/localGamesStore.ts
+++ client/src/utils/localGamesStore.ts
@@ -9,6 +9,24 @@ export const extractGameIdFromLocal = (gameId: string) => gameId.split('/')[1]
 
 // TODO: Handle local storage file size limits somehow
 
+const bufferToBase64 = (encodedBuffer: Buffer) => {
+	let binary = ''
+	const bytes = new Uint8Array(encodedBuffer)
+	const len = bytes.byteLength
+
+	for (let i = 0; i < len; i++) {
+		binary += String.fromCharCode(bytes[i])
+	}
+
+	const base64Buffer = btoa(binary)
+
+	return base64Buffer
+}
+
+const base64ToBuffer = (base64Buffer: string) => {
+	return Uint8Array.from(atob(base64Buffer), (c) => c.charCodeAt(0))
+}
+
 export const localGamesStore = {
 	getGame(gameId: string) {
 		const data = localStorage[LOCAL_GAMES_PREFIX + gameId]
@@ -17,11 +35,17 @@ export const localGamesStore = {
 			return null
 		}
 
-		return decode(data) as { state: GameState; config: GameConfig }
+		return decode(base64ToBuffer(data)) as {
+			state: GameState
+			config: GameConfig
+		}
 	},
 
 	setGame(gameId: string, data: { state: GameState; config: GameConfig }) {
-		localStorage[LOCAL_GAMES_PREFIX + gameId] = encode(stripUndefined(data))
+		const encodedBuffer = encode(stripUndefined(data))
+		const base64Buffer = bufferToBase64(encodedBuffer)
+
+		localStorage[LOCAL_GAMES_PREFIX + gameId] = base64Buffer
 	},
 
 	removeGame(gameId: string) {
