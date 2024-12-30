b4839608366e97a354a91dbcf1c401cd433b3ce1
Jan Zípek <jan@zipek.cz>
2024-12-30 09:15:03 +0100

    Restructure a bit

--- client/src/pages/Game/pages/Table/components/InYourFaceEvents/components/GameStartedEvent.tsx
+++ client/src/pages/Game/pages/Table/components/InYourFaceEvents/components/GameStartedEvent.tsx
@@ -24,93 +24,103 @@ export const GameStartedEvent = ({ event }: Props) => {
 			<SubTitle>Game Setup</SubTitle>
 
 			<Box gap="0.5rem" wrap="wrap">
-				<ClippedBox>
-					<ClippedBoxTitle $spacing>Players</ClippedBoxTitle>
-					<Box $p={2} wrap="wrap" justify="center" align="stretch" gap="0.5rem">
-						{event.players.map((player) => (
-							<Flex key={player.id} gap="0.25rem">
-								<FontAwesomeIcon icon={faUser} color={player.color} />
-								{player.name}
-							</Flex>
-						))}
-					</Box>
-				</ClippedBox>
-
-				{event.colonies && (
-					<ClippedBox>
-						<ClippedBoxTitle $spacing>Colonies</ClippedBoxTitle>
-						<Box $p={2} wrap="wrap" justify="center" align="stretch">
-							{event.colonies.map((c, index) => (
-								<ColonyDisplay
-									key={c.code}
-									index={index}
-									colony={c}
-									noActions
-								/>
-							))}
-						</Box>
-					</ClippedBox>
-				)}
-
-				<ClippedBox>
-					<ClippedBoxTitle $spacing>Milestones & Competitions</ClippedBoxTitle>
-					<Flex>
-						<Box
-							$p={2}
-							justify="center"
-							gap="0.25rem"
-							wrap="wrap"
-							align="stretch"
-						>
-							{game.map.milestones.map((m) => (
-								<MilestoneContainer key={m}>
-									<ClippedBox>
-										<ClippedBoxTitle $spacing>
-											{Milestones[m].title}
-										</ClippedBoxTitle>
-										<Box $p={2}>
-											<div>{Milestones[m].description}</div>
-											<Box $ml="auto">{Milestones[m].limit}</Box>
-										</Box>
-									</ClippedBox>
-								</MilestoneContainer>
-							))}
-						</Box>
-						<Box
-							$p={2}
-							justify="center"
-							gap="0.25rem"
-							wrap="wrap"
-							align="stretch"
-						>
-							{game.map.competitions.map((c) => (
-								<MilestoneContainer key={c}>
-									<ClippedBox>
-										<ClippedBoxTitle $spacing>
-											{Competitions[c].title}
-										</ClippedBoxTitle>
-										<Box $p={2}>{Competitions[c].description}</Box>
-									</ClippedBox>
-								</MilestoneContainer>
-							))}
-						</Box>
-					</Flex>
-				</ClippedBox>
-
-				{event.globalEvents && (
-					<Box $mt={2} direction="column" align="stretch">
+				<Flex gap="0.5rem" align="flex-start">
+					{event.colonies && (
 						<ClippedBox>
-							<ClippedBoxTitle $spacing>Global Events</ClippedBoxTitle>
-							<Box $p={2} justify="center">
-								<GlobalEventsDisplay
-									comingEvent={event.globalEvents.coming}
-									currentEvent={event.globalEvents.current}
-									distantEvent={event.globalEvents.distant}
-								/>
+							<ClippedBoxTitle $spacing>Colonies</ClippedBoxTitle>
+							<Box $p={2} wrap="wrap" justify="center" align="stretch">
+								{event.colonies.map((c, index) => (
+									<ColonyDisplay
+										key={c.code}
+										index={index}
+										colony={c}
+										noActions
+									/>
+								))}
 							</Box>
 						</ClippedBox>
-					</Box>
-				)}
+					)}
+
+					<div>
+						<ClippedBox>
+							<ClippedBoxTitle $spacing>Players</ClippedBoxTitle>
+							<Box
+								$p={2}
+								wrap="wrap"
+								justify="center"
+								align="stretch"
+								gap="0.5rem"
+							>
+								{event.players.map((player) => (
+									<Flex key={player.id} gap="0.25rem">
+										<FontAwesomeIcon icon={faUser} color={player.color} />
+										{player.name}
+									</Flex>
+								))}
+							</Box>
+						</ClippedBox>
+						<ClippedBox style={{ flex: 1 }}>
+							<ClippedBoxTitle $spacing>
+								Milestones & Competitions
+							</ClippedBoxTitle>
+							<Flex>
+								<Box
+									$p={2}
+									justify="center"
+									gap="0.25rem"
+									wrap="wrap"
+									align="stretch"
+								>
+									{game.map.milestones.map((m) => (
+										<MilestoneContainer key={m}>
+											<ClippedBox>
+												<ClippedBoxTitle $spacing>
+													{Milestones[m].title}
+												</ClippedBoxTitle>
+												<Box $p={2}>
+													<div>{Milestones[m].description}</div>
+													<Box $ml="auto">{Milestones[m].limit}</Box>
+												</Box>
+											</ClippedBox>
+										</MilestoneContainer>
+									))}
+								</Box>
+								<Box
+									$p={2}
+									justify="center"
+									gap="0.25rem"
+									wrap="wrap"
+									align="stretch"
+								>
+									{game.map.competitions.map((c) => (
+										<MilestoneContainer key={c}>
+											<ClippedBox>
+												<ClippedBoxTitle $spacing>
+													{Competitions[c].title}
+												</ClippedBoxTitle>
+												<Box $p={2}>{Competitions[c].description}</Box>
+											</ClippedBox>
+										</MilestoneContainer>
+									))}
+								</Box>
+							</Flex>
+						</ClippedBox>
+						{event.globalEvents && (
+							<Box $mt={2} direction="column" align="stretch">
+								<ClippedBox>
+									<ClippedBoxTitle $spacing>Global Events</ClippedBoxTitle>
+									<Box $p={2} justify="center">
+										<GlobalEventsDisplay
+											comingEvent={event.globalEvents.coming}
+											currentEvent={event.globalEvents.current}
+											distantEvent={event.globalEvents.distant}
+										/>
+									</Box>
+								</ClippedBox>
+							</Box>
+						)}
+					</div>
+				</Flex>
 			</Box>
 		</Container>
 	)
